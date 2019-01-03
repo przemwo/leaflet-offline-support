5c6cbb213e9c697546b9a7bfd384888ab852d56d
przemwo <przemwo@o2.pl>
2019-01-03 14:42:01 +0100

    playing with leaflet

--- main.js
+++ main.js
@@ -1,9 +1,60 @@
-const map = L.map('map').setView([54.3610063, 18.549947], 13); // lat, lng
+// Markers LatLng
+const markersLatLng = [
+    [54.3610063, 18.549947],
+    [54.0, 18.0],
+    [53.0, 19.0],
+];
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    maxZoom: 18,
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    id: 'mapbox.streets'
-}).addTo(map);
+const foo = L.latLngBounds(markersLatLng);
+// console.log(foo.getCenter());
+
+const map = L.map("map", {
+    center: [54.3610063, 18.549947], // lat, lng
+    zoom: 10,
+    //   maxBounds: markersLatLng,
+});
+
+// Event listeners
+const logEventType = (e) => console.log('EVENT: ', e.type);
+const handleLocationFound = (e) => {
+    logEventType(e);
+    console.log(e);
+};
+map.on("load", logEventType);
+map.on("viewreset", logEventType);
+map.on("layeradd", logEventType);
+map.on("moveend", logEventType);
+map.on('locationfound', handleLocationFound);
+
+const layer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+});
+
+layer.on("tileloadstart", () => {
+    console.log(111);
+});
+
+const markers = markersLatLng.map((markerLatLng) => L.marker(markerLatLng));
+
+markers.forEach((marker) => {
+    map.addLayer(marker);
+});
+
+map.addLayer(layer);
+
+setTimeout(() => {
+    map.fitBounds(foo, {
+    });
+}, 3000);
+
+setTimeout(() => {
+    map.locate({
+        setView: true,
+    });
+    console.log(map.getPixelBounds());
+}, 5000);
+
+console.log(map.getPixelOrigin());
+console.log(map.getPixelBounds());
+console.log(map.getPixelWorldBounds());
+console.log(layer)
